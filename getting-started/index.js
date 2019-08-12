dea643d39f63e30d1245b7946ad8c75c31a371d9
franciscochiarino <francisco.chiarino@gmail.com>
2019-08-12 15:59:30 +0200

    Exercise 6 Done

---
+++ getting-started/index.js
@@ -0,0 +1,39 @@
+//# 1.
+// let firstName = "Francisco";
+// let lastName = "Chiarino";
+// console.log(firstName, lastName);
+
+// 2.
+// let firstName = "Leonardo";
+// let lastName = "DiCaprio";
+// let city = "Los Angeles";
+// let job = "Actor";
+// console.log(firstName, lastName, city, job);
+
+// 3.
+// console.log(firstName, lastName + " lives in", city + " and work as an", job)
+
+// 4.
+// let length = 16.78; 
+// let lastName = "Johnson"; 
+// let car = ""; 
+// let isOpen = true;
+// console.log(typeof 16.78);
+// console.log(typeof "Johnson");
+// console.log(typeof car);
+// console.log(typeof true);
+
+// 5.
+// let ageMark = 21;
+// let ageJohn = "30";
+// console.log(typeof ageMark);
+// console.log(typeof ageJohn);
+
+// 6.
+// console.log((ageJohn > ageMark) ? "John is older than Mark" : "Mark is older than John");
+
+// 7.
+
+
+
+
