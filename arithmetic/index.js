46784ebb6ae35cbf1b3909c01b2c68a887f98c02
franciscochiarino <francisco.chiarino@gmail.com>
2019-08-13 14:50:48 +0200

    The Minimum Score

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -10,11 +10,17 @@
 // console.log((8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10);
 
 //Score
-// console.log ((75 + 70 + 85 + 90 + 100) / 5);
-const x = (85 * 6) - 75 - 70 - 85 - 90 - 100;
-const average = (75 + 70 + 85 + 90 + 100 + x) / 6;
-// console.log(x);
-console.log(average);
+// const x = (85 * 6) - 75 - 70 - 85 - 90 - 100;
+// const average = (75 + 70 + 85 + 90 + 100 + x) / 6;
+// console.log("Score in the sixth test: " + x);
+// console.log(average);
 
-// 3.
+// The Minimum Score
+const x = 78
+const result = 96
+const actualAverage = (8 * x) / 8;
+const minimumAverage = ((x * 8) + result) / 9;
+console.log(`James need a minimum of ${result}% to get an 80% average.`);
+
+
 
