6ddf8c495bbbcc20f6873e687a79a896ac473cb6
franciscochiarino <francisco.chiarino@gmail.com>
2019-08-13 14:23:19 +0200

    Add EX2

---
+++ arithmetic/index.js
@@ -0,0 +1,20 @@
+// 1.
+
+// const totalAmmount = 30
+// const someAmmount = 17
+// console.log((totalAmmount*someAmmount) / 100 + "%");
+
+// 2.
+
+// Working Hours
+// console.log((8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10);
+
+//Score
+// console.log ((75 + 70 + 85 + 90 + 100) / 5);
+const x = (85 * 6) - 75 - 70 - 85 - 90 - 100;
+const average = (75 + 70 + 85 + 90 + 100 + x) / 6;
+// console.log(x);
+console.log(average);
+
+// 3.
+
