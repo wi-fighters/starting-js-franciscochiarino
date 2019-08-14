5916a24c5f63004a9e8fd5249aaa4b475f7baf51
Francisco Chiarino <Kako@franciscos-mbp.localdomain>
2019-08-14 21:43:30 +0200

    String Methods Done

---
+++ string-methods/index.js
@@ -0,0 +1,73 @@
+
+// 1. Characters & Properties
+
+// const myName = "Francisco";
+// console.log(myName.length);
+// console.log(myName[1], myName[5]);
+
+// 2. JS 
+
+// const programmingLanguage = "JavaScript";
+// console.log(programmingLanguage.length);
+// console.log(programmingLanguage[0], programmingLanguage[4]);
+
+// . I can...
+
+// const statement = "I can walk in the park all day!";
+// console.log(statement.substring(18, 22));
+
+// 4. aSc
+
+// const programmingLanguage = "JavaScript";
+// console.log(programmingLanguage.substring(3, 6));
+
+// 5. Upper Case
+
+// const typicalPhrase = "Hello World";
+// console.log(typicalPhrase.toUpperCase());
+
+// 6. Lower Case
+
+// const earthlingSalute = "Hello Earthling";
+// console.log(earthlingSalute.toLocaleLowerCase());
+
+// 7. Does it contain...?
+
+// const compliment = "nice shoes";
+// console.log(compliment.includes("l"), compliment.includes("n"));
+
+// 8. Front & Back
+
+// const compliment = "nice shoes";
+// const newString = compliment[0] + compliment + compliment[0];
+// console.log(newString);
+
+// 9. Last Three Chars
+
+// const programmingLanguage = "JavaScript";
+// const newString = programmingLanguage.substring(7) + programmingLanguage + programmingLanguage.substring(7);
+// console.log(newString);
+
+// 10. Java
+
+// const statement = "Oh my god, I really like JavaScript";
+// console.log(statement.includes("Java"));
+
+// 11. First and Last
+
+// const earthlingSalute = "Hello Earthling";
+// const newString = earthlingSalute[earthlingSalute.length -1] + earthlingSalute.substring(1, 14) + earthlingSalute[0];
+// console.log(newString);
+
+// 12. Concatenation
+
+// const firstName = "Francisco";
+// const city = "Berlin";
+// const job = "delivery";
+// console.log(`Hello! My name is ${firstName}. I live in ${city} and I work as a ${job} guy.`)
+
+// 13. Capitalize
+
+// const story = "the quick brown fox";
+// console.log(story[0].toUpperCase() + story.substring(1));
+
